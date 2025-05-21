ee707b9395e2b3a5b82dfbe019cd81b0b71dd681
eli <liskoviche@gmail.com>
2025-05-21 16:29:22 -0400

    Added Card class. makeImage is not yet created.

---
+++ src/blackjack/Card.java
@@ -0,0 +1,78 @@
+package blackjack;
+
+public class Card {
+
+	static enum Suit {
+		HEARTS, DIAMONDS, SPADES, CLUBS
+	};
+	
+	private Suit suit;
+	private int value; // 1 is an ace, 2-10 is the number, 11 is j, 12 is q, 13 is k
+	private int face; // 1 is j, 2 is q, 3 is k, 0 is nothing
+	
+	public Card(Suit suit, int value) {
+		this.suit = suit;
+		
+		// have to translate card value to face+real value
+		
+		if (value <= 10) {
+			this.value = value;
+			this.face = 0;
+		} else {
+			this.value = 10;
+			this.face = value-10;
+		}
+	}
+	
+	@Override
+	public String toString() {
+		
+		String result = "";
+		
+		switch(face){
+		case 1:
+			result += "jack";break;
+		case 2:
+			result += "queen";break;
+		case 3:
+			result += "king";break;
+		default:
+			break;
+		}
+		
+		return result+" of "+suit.toString().toLowerCase();
+		
+	}
+	
+	public Suit getSuit() {
+		return suit;
+	}
+	
+	public void setSuit(Suit suit) {
+		this.suit = suit;
+	}
+	
+	public int getValue() {
+		return value;
+	}
+	
+	public void setValue(int value) {
+		this.value = value;
+	}
+	
+	public int getFace() {
+		return face;
+	}
+	
+	public void setFace(int face) {
+		this.face = face;
+	}
+	
+	/*
+	 * makeImage method:
+	 * Generate an image through checking face, value, and suit to avoid having 52 files
+	 * copy the craps dice draw method
+	 */
+	
+	
+}
