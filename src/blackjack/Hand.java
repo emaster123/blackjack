d33bc1f5930a96b03b509e2777e9d7d7ae760ace
eli <liskoviche@gmail.com>
2025-05-26 12:50:21 -0400

    Added part of hand

--- src/blackjack/Hand.java
+++ src/blackjack/Hand.java
@@ -7,4 +7,25 @@ public class Hand {
 	private ArrayList<Card> hand;
 	private int aces;
 
+	public void take(Card card) {
+		hand.add(card);
+	}
+	
+	public ArrayList<Card> getHand() {
+		return hand;
+	}
+	
+	public int[] getScores() {
+		
+		int[] scores = new int [aces+1];
+		int acesCounted = aces;
+		
+		for (int score : scores) {
+			
+			
+			
+		}
+		
+ 	}
+	
 }
